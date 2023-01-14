0c6559357aab5ab74cc29d328f7485e252f44c1b
Shiielty <56679623+Shiielty@users.noreply.github.com>
2023-01-14 11:00:26 +0700

    finished caesarCipher test

--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -1,4 +1,20 @@
 import { caesarChiper } from './caesarCipher'
 
-test('fail test', () => expect(123).toBe(321))
-test('pass test', () => expect(123).toBe(123))
+test('CaesarChiper test: only alphabet', () => {
+  expect(caesarChiper('abcdefghijklmnopqrstuvwxyz', 3))
+    .toBe('defghijklmnopqrstuvwxyzabc')
+  expect(caesarChiper('AbcDefghijKlmnOpqRstuvwxyZ', 3))
+    .toBe('DefGhijklmNopqRstUvwxyzabC')
+})
+
+test('CaesarChiper test: alphanumeric', () => {
+  expect(caesarChiper('404meansNotFound', 9))
+    .toBe('404vnjwbWxcOxdwm')
+})
+
+test('CaesarChiper test: alphanumeric with punctuation', () => {
+  expect(caesarChiper('Hello, world!', 5))
+    .toBe('Mjqqt, btwqi!')
+  expect(caesarChiper('He said, "The score is 98!"', 5))
+    .toBe('Mj xfni, "Ymj xhtwj nx 98!"')
+})
